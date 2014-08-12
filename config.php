8940eba30f11ab82a8686dc0d2041089416f1e26
j4ck <rustyj4ck@gmail.com>
2014-08-12 10:20:27 +0400

    config changes

--- config.php
+++ config.php
@@ -1,10 +1,15 @@
 <?php
 return array(
 
+     // Default scan path is `script root`
 	 // 'root'   => '/root/to/scan',
+     
+     // ServerTag for text reports, default _SERVER[SERVER_NAME]
 	 // 'server' => 'server_name',
  
 	 'files' => '(\.php.?|\.htaccess)$',
+     
+     'log' => true,
 
 	 'mail' => array(
 	 	'from'   => 'info@skillz.ru',
